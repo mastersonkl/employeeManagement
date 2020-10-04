63ccf87b054185aa62656a6049469a931d69b404
Kendall Masterson <masterson.kendall@gmail.com>
2020-10-03 20:32:58 -0400

    updated file structure

--- index.js
+++ index.js
@@ -33,3 +33,21 @@ function pushDept() {
       }
     });
   }
+
+
+  //connection.js
+//   const util = require("util");
+// const mysql = require("mysql");
+// const connection = mysql.createConnection({
+//   host: "localhost",
+//   // Your username
+//   user: "root",
+//   // Your password
+//   password: "@Gail123",
+//   database: "employee_tracker"
+// });
+// connection.connect();
+// // Setting up connection.query to use promises instead of callbacks
+// // This allows us to use the async/await syntax
+// connection.query = util.promisify(connection.query);
+// module.exports = connection;
